7a2be27f7863864e10443378c201d01e7acefb7b
nadula-w <naduwathura@gmail.com>
2025-01-23 00:43:37 +0530

    nadula

--- src/printBill.java
+++ src/printBill.java
@@ -7,6 +7,7 @@ public class printBill extends Billing{
     double Qty;
     double Total;
     static int invoiceno;
+    static int totalbill=0;
     printBill(String itemName, String bid, double Qty, double Total){
         this.itemName=itemName;
         this.bid=bid;
@@ -32,13 +33,14 @@ public class printBill extends Billing{
     }
 
     public static void printHeader() {
-        System.out.println("\t\t No-50,Kandy Road,Kiribathgoda \t\t");
+
+        System.out.println("\t\t\t No-50,Kandy Road,Kiribathgoda \t\t\t");
         System.out.println("\tTEL:-0112-918366/0125");
         System.out.println("\tCashier : " + Main.uname);
         invoiceno = (int) (Math.random() * 1000);
         System.out.printf("\tInvoice no: %d", (int) invoiceno);
         System.out.println();
-        System.out.println("\t------------------------------------------\t");
+        System.out.println("\t-------------------------------------------------\t");
         System.out.println("\tName\t\tPrice\t\tQty\t\tNett Price");
         System.out.println("\t------------------------------------------\t");
     }
@@ -53,7 +55,14 @@ public class printBill extends Billing{
                 while (rsc.next()) {
                     System.out.println("\t"+rsc.getString("Itemname")+"\t\t"+rsc.getDouble("unitprice")+
                             "\t\t"+rsc.getInt("Qty")+"\t\t"+ rsc.getDouble("unitprice")*rsc.getInt("Qty"));
+
+                    int net= (int) (rsc.getDouble("unitprice")*rsc.getInt("Qty"));
+                     totalbill+=net;
                 }
+            System.out.println("\t------------------------------------------\t");
+            System.out.printf("Total\t\t\t\t\t\t\t\t\t\t%d",totalbill);
+            System.out.println("\t------------------------------------------\t");
+
         }
 
     }
